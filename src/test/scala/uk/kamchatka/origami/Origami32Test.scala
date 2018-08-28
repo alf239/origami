209202b5ba8d297fa679e5f6ae09ffc2d5340c34
Alexey Filippov <afilippov@acm.org>
2018-08-28 16:40:28 +0100

    Exercise 3.4

--- src/test/scala/uk/kamchatka/origami/Origami32Test.scala
+++ src/test/scala/uk/kamchatka/origami/Origami32Test.scala
@@ -1,9 +1,16 @@
 package uk.kamchatka.origami
 
+import org.scalacheck.Arbitrary
+import org.scalatest.prop.Checkers
+import org.scalacheck.Gen
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Prop._
 import org.scalatest.FlatSpec
 import uk.kamchatka.origami.Origami32._
 
-class Origami32Test extends FlatSpec {
+import scala.annotation.tailrec
+
+class Origami32Test extends FlatSpec with Checkers {
   "foldL" should "calculate" in {
     assert(foldL[Double, Double](_ + _)(0.0, list(1.0, 2.0, 3.0)) == 6.0)
   }
@@ -20,5 +27,25 @@ class Origami32Test extends FlatSpec {
   "concatL" should "flatten" in {
     assert(concatL(list(list(1, 2), list(3, 4), list(5), list(6), Nil)) ==
       list(1, 2, 3, 4, 5, 6))
+  }
+
+  "isort" should "sort" in {
+    check((n: List[Int]) => sorted(isort(n)))
+  }
+
+  @tailrec
+  final def sorted[A](ys: List[A])(implicit ord: Ordering[A]): Boolean = ys match {
+    case Nil | Cons(_, Nil) => true
+    case Cons(x, Cons(x1, xs: List[A])) => ord.lteq(x, x1) && sorted(Cons(x1, xs))
+  }
+
+
+  implicit def arbitraryList[A: Arbitrary]: Arbitrary[List[A]] = Arbitrary {
+    Gen.oneOf(
+      Gen.const(Nil),
+      for {
+        head <- arbitrary[A]
+        tail <- arbitraryList.arbitrary
+      } yield Cons(head, tail))
   }
 }
